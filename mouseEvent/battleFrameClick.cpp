4f0c427be3ca13806112ee4189bfcb6ba570e28f
prunus520 <prunus520@gmail.com>
2020-04-23 16:17:32 +0800

    fix: hitPlayerOrComputer's mouse click XY

--- mouseEvent/battleFrameClick.cpp
+++ mouseEvent/battleFrameClick.cpp
@@ -5,10 +5,10 @@ void battleFrameClick(int state, int x, int y){
 }
 
 void hitPlayerOrComputer(int state, int x, int y){
-	if (state == 1 && mouse.getClickX() >= 800 && mouse.getClickX() <= 1340 &&
-			mouse.getClickY() >= 310 && mouse.getClickY() <= 850 && player_computer_sleep && palyer_down){
-		computer.setRow((mouse.getClickY() - 310) / 67.5);
-		computer.setColumn((mouse.getClickX() - 800) / 67.5);
+	if (state == 1 && x >= 800 && x <= 1340 &&
+			y >= 310 && y <= 850 && player_computer_sleep && palyer_down){
+		computer.setRow((y - 310) / 67.5);
+		computer.setColumn((x - 800) / 67.5);
 		player_computer_flag = true;
 		palyer_init = true;
 	}
