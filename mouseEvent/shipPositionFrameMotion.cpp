47aa4278595c2e6b2c8fa84f3847602c78db57c5
prunus520 <prunus520@gmail.com>
2020-04-24 13:58:32 +0800

    fix: motionShip caused window to crash

--- mouseEvent/shipPositionFrameMotion.cpp
+++ mouseEvent/shipPositionFrameMotion.cpp
@@ -1,12 +1,11 @@
 #include "shipPositionFrameMotion.h"
 
 void shipPositionFrameMotion(){
-//	¾É­Pclicked¼Æ­È²§±` 
 	motionShip();
 }
 
 void motionShip(){
-	if (frame == SHIP_POSITION_FRAME && mouse.getMouseClicked() == 1){
+	if (frame == SHIP_POSITION_FRAME && shipMOVE != -1){
 		ship[shipMOVE].changeOldShipPositionByNewShipPosition();
 		shipMOVE = -1;
 	}
